1d537d691767f0756c3ad41e5e2e7bf91fe24887
Biswash Adhikari <biswash.adhikari@bison.howard.edu>
2019-03-04 09:59:06 -0800

    initiatlized project

---
+++ shareEat/shareEat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  shareEat
+//
+//  Created by Biswash Adhikari on 3/4/19.
+//  Copyright © 2019 Biswash Adhikari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
